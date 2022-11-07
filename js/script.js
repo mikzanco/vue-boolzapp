5f68b5d1fcbb27d9a69ce18a1d3abe114f9f5e21
mikzanco <mikyzancolo97@gmail.com>
2022-11-07 10:41:26 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    },
+    created(){
+
+    }
+}).mount('#app')
