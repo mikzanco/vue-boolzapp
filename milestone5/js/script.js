c7f8825112111a06ab3b2cb95048afea43abfbc9
mikzanco <mikyzancolo97@gmail.com>
2022-11-09 16:13:32 +0100

    create milestone5

---
+++ milestone5/js/script.js
@@ -0,0 +1,229 @@
+const { createApp } = Vue;
+// const DateTime = luxon.DateTime;
+// const now = DateTime.now();
+// console.log(now);
+createApp({
+    data(){
+        
+        return{
+            newMessageString: '',
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                    
+                {
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Alessandro B.',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro L.',
+                    avatar: '_5',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ricordati di chiamare la nonna',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Va bene, stasera la sento',
+                            status: 'received'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Claudia',
+                    avatar: '_6',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao Claudia, hai novità?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Non ancora',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'Nessuna nuova, buona nuova',
+                            status: 'sent'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Federico',
+                    avatar: '_7',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            status: 'received'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Davide',
+                    avatar: '_8',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'OK!!',
+                            status: 'received'
+                        }
+                    ],
+                }
+            ],
+            activeChat: 0,
+            // stringa vuota di search presa dal v-model dell'input
+            search: '',
+                    
+        }
+    },
+    methods:{
+        changeChat(index){
+            this.activeChat = index;
+        },
+        createMessage(){
+            // creo il messagggio dell'utente generato dal campo di input text e inviato con un @keyup.enter
+            const newMessage ={
+                message: this.newMessageString,
+                status: 'sent',
+            }
+            this.contacts[this.activeChat].messages.push(newMessage);
+            this.newMessageString = '';
+            setTimeout(this.replyMessage, 3000)
+        },
+        replyMessage(){
+            // creo il messaggio di risposta automatico.
+            const newMessage={
+                message: 'ok!',
+                status: 'received',
+            }
+            this.contacts[this.activeChat].messages.push(newMessage);
+        },
+        findChat(){
+            // al click di un tasto la funzione parte
+            // fa il ciclo delle chat che inizialemnte sono tutte visibili.
+
+            this.contacts.forEach(utente => {
+                utente.visible = true;
+                // se il nome dell'utente NON include la o le lettere  sctitte nell'input allora il visible di quel utente diventrea false. Per il v-show scritto nel html (v-show=?contacts.visible') ci ritorna una booleana, se true il v-show mostra. se false il v-show cancella. 
+                if(!utente.name.toLowerCase().includes(this.search.toLowerCase())){
+                    utente.visible = false;
+                }
+            });
+           
+
+        },
+        
+    },
+    mounted(){
+
+    },
+    created(){
+
+    }
+}).mount('#app')
